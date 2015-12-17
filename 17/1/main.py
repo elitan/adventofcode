c94b57b6d6c889be5923a064de5c0d6b136ea066
elitasson <johan@eliasson.me>
2015-12-17 09:26:15 +0100

    17 part one done

---
+++ 17/1/main.py
@@ -0,0 +1,17 @@
+import itertools
+
+def gen(containers):
+	i = 1
+	while i < 2**len(containers):
+		yield map(int, list("{0:b}".format(i).zfill(len(containers))))
+		i += 1
+
+containers = map(int, [ f.rstrip() for f in open('input')])
+
+p1 = 0
+for c in gen(containers):
+	s = [a*b for a,b in zip(containers,c)]
+	if sum(s) == 150:
+		p1 += 1
+
+print(p1)
