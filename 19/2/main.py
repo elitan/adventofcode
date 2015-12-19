66eae9797bb0ec6a231e6d7dbfe7712217a6b242
elitan <johan@eliasson.me>
2015-12-19 21:33:57 +0100

    part 2 algorithm done, but to slow?! :<

--- 19/2/main.py
+++ 19/2/main.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import time
 
 def replaceNth(s, a, b, n):
 	start = -1
@@ -7,11 +8,28 @@ def replaceNth(s, a, b, n):
 		start = s.find(a, start+1)
 	return s[:start] + b + s[start+len(a):]
 
-def walk(s):
+def walk(s, depth):
 	global map_table
-	for find, replace in map_table:
-		a = re.findall(ur'(%s)' % )
-	pass
+	global correct_depth
+
+	#print("%d, inside walk with :: %s" % (depth, s))
+	#time.sleep(0.1)
+
+	# base
+	if s == 'e':
+		print("DEPTH: ", depth)
+		correct_depth.add(depth)
+		sys.exit()
+		return
+
+	if 'e' in s:
+		return
+
+	# recursion
+	for find in map_table:
+		for i in xrange(s.count(find)):
+			s_new = replaceNth(s, find, map_table[find], i+1)
+			walk(s_new, depth + 1)
 
 start_input = open('input').read()
 
@@ -22,4 +40,8 @@ for line in open('mapping'):
 	map_table[replace] = find;
 
 
+correct_depth = set()
+walk(start_input, 0)
 
+print(correct_depth)
+print("lowest depth: ", min(correct_depth))
