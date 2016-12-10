b52e220ee67997f3d32fcc695df92b5667077932
elitan <johan@eliasson.me>
2016-12-10 16:38:30 +0100

    part 2 working

--- 2016/10/main-part1.py
+++ 2016/10/main-part1.py
@@ -15,9 +15,17 @@ def add_value_to_bot(bot_n, value, bot_container):
         bot_container[bot_n] = [value]
     bot_container[bot_n].sort()
 
+def add_value_to_output(output_n, value, output_container):
+    value = int(value)
+    if output_n in output_container:
+        output_container[output_n].append(value)
+    else:
+        output_container[output_n] = [value]
+
 def init_bot_container(bot_n, bot_container):
     if bot_n not in bot_container:
         bot_container[bot_n] = []
+
 
 def print_bot_container(bot_container, bots):
     chip_n = []
@@ -33,6 +41,7 @@ def print_bot_container(bot_container, bots):
 bots = []
 bot_instruction = {}
 bot_container = {}
+output_container = {}
 
 
 input_file = 'input'
@@ -65,13 +74,6 @@ with open(input_file, 'r') as f:
                 bots.append(giver_bot_n)
             continue
 
-print(bot_container)
-print(bot_instruction)
-print(bots)
-print('')
-print('')
-print('')
-
 for bot_n in bots:
     init_bot_container(bot_n, bot_container)
 
@@ -79,35 +81,38 @@ match_container = [17, 61]
 if test:
     match_container = [2, 3]
 
-while True:
+c = True
+while c:
+    c = False
     for bot_n in bots:
         if len(bot_container[bot_n]) != 2:
             continue
+        c = True
 
-        print('do instructions for bot n: ', bot_n)
         bot_container[bot_n].sort()
         if bot_container[bot_n] == match_container:
-            print(bot_container[bot_n], match_container)
             print('FOUND!', bot_n)
-            sys.exit()
 
         # do instruction
         lower_taker_type = bot_instruction[bot_n]['lower_taker_type']
         lower_taker_n = bot_instruction[bot_n]['lower_taker_n']
         higher_taker_type = bot_instruction[bot_n]['higher_taker_type']
         higher_taker_n = bot_instruction[bot_n]['higher_taker_n']
-        print(lower_taker_type, lower_taker_n, higher_taker_type, higher_taker_n)
 
         # handle lowest value
         if lower_taker_type == 'bot':
             add_value_to_bot(lower_taker_n, bot_container[bot_n][0], bot_container)
+        else:
+            add_value_to_output(lower_taker_n, bot_container[bot_n][0], output_container)
 
         # handle highest value
         if higher_taker_type == 'bot':
             add_value_to_bot(higher_taker_n, bot_container[bot_n][-1], bot_container)
+        else:
+            add_value_to_output(higher_taker_n, bot_container[bot_n][-1], output_container)
 
         # reset bots container
         bot_container[bot_n] = []
 
-        print_bot_container(bot_container, bots)
-        input('')
+
+print(output_container['0'][0] * output_container['1'][0] * output_container['2'][0])
