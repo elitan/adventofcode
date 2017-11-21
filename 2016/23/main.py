b85f5e1b1a597f51cec1ff59bf4ddec0069b69ef
elitan <johan@eliasson.me>
2017-11-21 23:31:18 +0100

    update, stuck in inf. loop

--- 2016/23/main.py
+++ 2016/23/main.py
@@ -4,14 +4,14 @@ import sys
 re_cpy = re.compile(r'cpy (\d+|\w+) (\w+)')
 re_inc = re.compile(r'inc (\w+)')
 re_dec = re.compile(r'dec (\w+)')
-re_jnz = re.compile(r'jnz (\d+|\w+) (-?\d+)')
+re_jnz = re.compile(r'jnz (\w+) (-?\d+)')
 re_tgl = re.compile(r'tgl (\w+)')
 
 with open('input', 'r') as f:
 	instructions  = [line.rstrip() for line in f]
 
 register = {
-	'a': 0,
+	'a': 7,
 	'b': 0,
 	'c': 0,
 	'd': 0,
@@ -21,13 +21,20 @@ fp = 0
 def toggle_instruction(reg, fp, register, instructions):
 
 	# toggle instruction
-	line_to_toggle = instructions[fp + register[reg]]
+	try:
+		line_to_toggle = instructions[fp + register[reg]]
+	except:
+		return
 
 	# self toggle
 	if register[reg] == 0:
 		print('self toggle')
 		line_to_toggle = 'inc {}'.format(reg)
-		instructions[fp + register[reg]] = line_to_toggle
+		try:
+			instructions[fp + register[reg]] = line_to_toggle
+		except:
+			pass
+
 		return
 
 	if line_to_toggle.count(' ') == 1:
@@ -37,23 +44,38 @@ def toggle_instruction(reg, fp, register, instructions):
 		else:
 			line_to_toggle = 'inc {}'.format(line_to_toggle[4:])
 
-		instructions[fp + register[reg]] = line_to_toggle
+		try:
+			instructions[fp + register[reg]] = line_to_toggle
+		except:
+			pass
+
 		return
-	elif line_to_toggle.count(' ') == 2:
+
+	if line_to_toggle.count(' ') == 2:
 		print('two arguments')
 		print(line_to_toggle)
 		if line_to_toggle.startswith('jnz'):
 			line_to_toggle = 'cpy {}'.format(line_to_toggle[4:])
 		else:
 			line_to_toggle = 'jnz {}'.format(line_to_toggle[4:])
-		instructions[fp + register[reg]] = line_to_toggle
+
+		try:
+			instructions[fp + register[reg]] = line_to_toggle
+		except:
+			pass
+
+		return
 
 while fp < len(instructions):
 
 	instruction = instructions[fp]
 
 	if instruction.startswith('cpy'):
-		a, reg = re.findall(re_cpy, instruction)[0]
+		try:
+			a, reg = re.findall(re_cpy, instruction)[0]
+		except:
+			fp += 1
+			continue
 		if a.isdigit():
 			register[reg] = int(a)
 		else:
@@ -68,7 +90,11 @@ while fp < len(instructions):
 		register[reg] -= 1
 
 	if instruction.startswith('jnz'):
-		a, rel = re.findall(re_jnz, instruction)[0]
+		try:
+			a, rel = re.findall(re_jnz, instruction)[0]
+		except:
+			fp += 1
+			continue
 		if a.isdigit():
 			c = int(a)
 		else:
@@ -82,10 +108,11 @@ while fp < len(instructions):
 		reg = re.findall(re_tgl, instruction)[0]
 		toggle_instruction(reg, fp, register, instructions)
 
-	# print(instruction)
-	# print(register)
-	# for instruction in instructions:
-	# 	print(instruction)
+	print(instruction)
+	print(register)
+	print(fp)
+	for instruction in instructions:
+		print(instruction)
 	# input('next tick')
 	fp += 1
 
