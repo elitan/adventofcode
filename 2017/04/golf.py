d62d5a0ee22b7ab0eb4864f59a6151f94dd516d5
elitan <johan@eliasson.me>
2017-12-04 12:26:51 +0100

    improvements

--- 2017/04/golf.py
+++ 2017/04/golf.py
@@ -1,2 +1,2 @@
-print(sum([len(x.rstrip().split()) == len(set(x.rstrip().split())) for x in open('input')]))
-print(sum([len([''.join(sorted(y)) for y in x.rstrip().split()]) == len(set([''.join(sorted(y)) for y in x.rstrip().split()])) for x in open('input')]))
+print(sum(len(x.rstrip().split()) == len(set(x.rstrip().split())) for x in open('input')))
+print(sum(len([''.join(sorted(y)) for y in x.rstrip().split()]) == len(set([''.join(sorted(y)) for y in x.rstrip().split()])) for x in open('input')))
