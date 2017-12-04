9200f20e00de04d4c50f0d9c435f7b97673e2259
elitan <johan@eliasson.me>
2017-12-04 09:33:41 +0100

    done

---
+++ 2017/04/main.py
@@ -0,0 +1,71 @@
+def validPassphraseP1(passphrase):
+
+    passphrase = passphrase.split(' ')
+
+    words = set()
+
+    for word in passphrase:
+        words_size = len(words)
+
+        # add the word to words set.
+        # if the word was already in the words set, the length is the same as
+        # before
+        words.add(word)
+
+        # test if invalid passphrase
+        if words_size == len(words):
+            return False
+
+    return True
+
+def validPassphraseP2(passphrase):
+
+    passphrase = passphrase.split(' ')
+
+    words = set()
+
+    for word in passphrase:
+        words_size = len(words)
+
+        # sort the chars in each word
+        words.add(''.join(sorted(list(word))))
+
+        # test if invalid passphrase
+        if words_size == len(words):
+            return False
+
+    return True
+
+def solve():
+
+    a1 = 0
+    a2 = 0
+    fh = open('input')
+    valid_passphrases = 0
+
+    for line in fh:
+
+        # get passphrase
+        passphrase = line.rstrip()
+
+        if validPassphraseP1(passphrase):
+            a1 += 1
+
+        if validPassphraseP2(passphrase):
+            a2 += 1
+
+    fh.close()
+
+    return a1, a2
+
+
+def main():
+
+    a1, a2 = solve()
+
+    print('part 1: ', a1)
+    print('part 2: ', a2)
+
+
+if __name__ == '__main__':
+    main()
