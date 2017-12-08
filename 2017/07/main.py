4b184ed142dbe84b56ca4b0e64e5abddab22e0a0
elitan <johan@eliasson.me>
2017-12-08 14:50:04 +0100

    done

--- 2017/07/main.py
+++ 2017/07/main.py
@@ -1,12 +1,12 @@
 import sys
+from collections import defaultdict
 
-
-def main():
+def part1(input_file_name):
 
     nodes = set()
     below_nodes = set()
 
-    for line in open('input'):
+    for line in open(input_file_name):
 
         line = line.rstrip().split(' ')
 
@@ -16,8 +16,91 @@ def main():
             n = n.replace(',', '')
             below_nodes.add(n)
 
-    print(nodes - below_nodes)
+    return list(nodes - below_nodes)[0]
 
+def getWeight(nodes, current_node_name, depth_weights, depth):
+
+    weight, children = nodes[current_node_name]
+
+    total_weight = weight
+
+    child_weights = []
+    child_weights_dict = {}
+
+    for child in children:
+        w = getWeight(nodes, child, depth_weights, (depth + 1))
+        total_weight += w
+        child_weights.append(w)
+        child_weights_dict[child] = w
+
+    # if child_weights and not balanced
+    if child_weights and child_weights[0] * len(child_weights) != sum(child_weights):
+
+        # since we found the unbalancing, we will exit the program inside this
+        # if statement
+
+        # find the correct and wrong weight
+        for child_weight in child_weights:
+            if child_weights.count(child_weight) == 1:
+                wrong_weight = child_weight
+            if child_weights.count(child_weight) > 1:
+                correct_weight = child_weight
+
+        # find node that is wrong
+        for child_name, weight in child_weights_dict.items():
+
+            if weight == wrong_weight:
+                wrong_node_name = child_name
+                break
+
+        # find the diff
+        diff = correct_weight - wrong_weight
+
+        # now calc the correct value for the 'wrong' node
+        ret = nodes[wrong_node_name][0] + diff
+
+        print('answer: ', ret)
+        sys.exit()
+
+        return ret
+
+    depth_weights[depth].append(total_weight)
+
+    return total_weight
+
+def part2(input_file_name):
+
+    nodes = {}
+
+    for line in open(input_file_name):
+
+        line = line.rstrip().split(' ')
+
+        name = line[0]
+        weight = int(line[1][1:-1]) # remove parenteces
+
+        nodes_children = []
+        nodes[name] = (weight, nodes_children)
+
+        for n in line[3:]:
+
+            n = n.replace(',', '')
+
+            nodes_children.append(n)
+
+    start_node_name = part1(input_file_name)
+
+    depth_weights = defaultdict(list)
+
+    weight = getWeight(nodes, start_node_name, depth_weights, 0)
+
+
+def main():
+
+    input_file_name = 'input'
+
+    print('part 1:', part1(input_file_name))
+    print('part 2:', part2(input_file_name))
 
 if __name__ == '__main__':
     main()
