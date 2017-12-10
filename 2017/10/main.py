10a4bc5fc41078c7a7fc53107d2eaaa84aa8b561
elitan <johan@eliasson.me>
2017-12-10 21:40:27 +0100

    part 1 done

--- 2017/10/main.py
+++ 2017/10/main.py
@@ -12,8 +12,10 @@ class CircularList:
         return len(self.list)
 
     def __getitem__(self, key):
+
         if type(key) is int:
             return self.list[key]
+
         elif type(key) is slice:
 
             start = key.start % len(self.list)
@@ -22,8 +24,9 @@ class CircularList:
 
             if start < stop:
                 return self.list[start:stop]
-
-            else:
+            elif start == stop:
+                return []
+            else: # start > stop
                 return self.list[start:] + self.list[:stop]
 
     def __setitem__(self, key, value):
@@ -33,35 +36,26 @@ class CircularList:
 
         elif type(key) is slice:
 
+            # nothing to add
+            if not value:
+                return
+
             start = key.start % len(self.list)
             stop = key.stop % len(self.list)
             step = key.step
-
-            print('set')
-            print('start: ', start, ' , stop: ', stop)
 
             if start < stop:
                 self.list[start:stop] = value
 
             elif start == stop:
-
-                print('front: ', value[-start:])
-                print('back: ', value[:-start])
-
                 self.list = value[-start:] + value[:-start]
 
-            else:
-                # you can only substitute len(value) elements in self.list
-                # with this calss plz
-                #
+            else: # start > stop
+
                 back_list_start = len(self.list) - start
                 back_list = value[:back_list_start]
-                print('back list: ', back_list)
 
                 front_list = value[back_list_start:]
-                print('front list: ', front_list)
-
-                print('middle list: ', self.list[stop:start], len(self.list[stop:start]))
 
                 self.list = front_list + self.list[stop:start] + back_list
 
@@ -73,10 +67,8 @@ class CircularList:
 
 def main():
 
-    input_file_name = 'input2'
+    input_file_name = 'input'
     input_list_length = 256
-    input_file_name = 'test'
-    input_list_length = 5
 
     circular_list = CircularList()
 
@@ -85,37 +77,25 @@ def main():
     for x in range(input_list_length):
         circular_list.append(x)
 
-
     current_position = 0
     skip_size = 0
 
     for current_length in input_lengths:
-        print('')
-        print('circular_list: ', circular_list)
-        if len(circular_list) != input_list_length:
-            print('wrong')
-            sys.exit()
-        if (len(circular_list)) != len(set(circular_list)):
-            print('wrong 2')
-            sys.exit()
 
+        # where to cut?
         start = current_position
         stop = start + current_length
 
+        # extract and reverse sublist
         sublist = circular_list[start:stop]
-        print('sublist: ', sublist)
         sublist = sublist[::-1]
-        print('sublist reversed: ', sublist)
 
+        # put it back in
         circular_list[start:stop] = sublist
 
+        # increase position and skip size
         current_position += current_length + skip_size
-        current_position = current_position % input_list_length
         skip_size += 1
-        print('skip size: ', skip_size)
-        input()
-
-    print('final circular_list: ', circular_list)
 
     print('answer: ', circular_list[0] * circular_list[1])
 
