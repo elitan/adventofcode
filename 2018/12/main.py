f883eee5228340b4992de7c5822041320bf49708
elitan <johan@eliasson.me>
2018-12-12 15:16:22 +0100

    done

--- 2018/12/main.py
+++ 2018/12/main.py
@@ -4,54 +4,25 @@ from collections import defaultdict
 
 
 def transition(state, i):
-
 	transition_data = {
-		'...##': '#',
-		'..#..': '#',
-		'.#...': '#',
-		'.#.#.': '#',
-		'.#.##': '#',
-		'.##..': '#',
-		'.####': '#',
-		'#.#.#': '#',
-		'#.###': '#',
-		'##.#.': '#',
-		'##.##': '#',
+		'#####': '#',
+		'####.': '#',
 		'###..': '#',
 		'###.#': '#',
-		'####.': '#',
-		# '#..#.': '.',
-		# '##...': '#',
-		# '#....': '.',
-		# '#...#': '#',
-		# '...#.': '.',
-		# '.#..#': '#',
-		# '#.#.#': '.',
-		# '.....': '.',
-		# '##.##': '#',
-		# '##.#.': '#',
-		# '###..': '#',
-		# '#.##.': '.',
-		# '#.#..': '#',
-		# '##..#': '#',
-		# '..#.#': '#',
-		# '..#..': '.',
-		# '.##..': '.',
-		# '...##': '#',
-		# '....#': '.',
-		# '#.###': '#',
-		# '#..##': '#',
-		# '..###': '#',
-		# '####.': '#',
-		# '.#.#.': '#',
-		# '.####': '.',
-		# '###.#': '#',
-		# '#####': '#',
-		# '.#.##': '.',
-		# '.##.#': '.',
-		# '.###.': '.',
-		# '..##.': '.',
-		# '.#...': '#',
+		'##...': '#',
+		'##..#': '#',
+		'##.##': '#',
+		'##.#.': '#',
+		'#.###': '#',
+		'#..##': '#',
+		'#...#': '#',
+		'#.#..': '#',
+		'.#.#.': '#',
+		'.#...': '#',
+		'.#..#': '#',
+		'..#.#': '#',
+		'...##': '#',
+		'..###': '#',
 	}
 
 	a = ''
@@ -62,15 +33,15 @@ def transition(state, i):
 		return '.'
 
 
-def printState(state, left_most, right_most):
-	for i in range(left_most, right_most + 1):
-		print(state[i], end='')
+def printState(state):
+	state_keys = list(state.keys())
+	for key in range(min(state_keys) - 2, max(state_keys) + 2):
+		print(state[key], end='')
 	print('')
 
 def main():
 
 	input_data = '.#####.##.#.##...#.#.###..#.#..#..#.....#..####.#.##.#######..#...##.#..#.#######...#.#.#..##..#.#.#'
-	input_data = '#..#.#..##......###...###'
 	state = defaultdict(lambda: '.')
 
 	left_most = float('inf')
@@ -81,24 +52,22 @@ def main():
 			left_most = min(left_most, i)
 			right_most = max(right_most, i)
 
-	print(state)
-	print(left_most, right_most)
-
-	printState(state, left_most, right_most)
-
-	p1 = 0
-	for i in range(21):
+	prev_p1 = 0
+	for i in range(200):
 		new_state = defaultdict(lambda: '.')
-		for i in range(left_most-10, right_most + 20):
-			c = transition(state, i)
-			p1 += 1 if c == '#' else 0
-			new_state[i] = c
+		p1 = 0
+		state_keys = list(state.keys())
+		for key in range(min(state_keys) - 2, max(state_keys) + 2):
+			c = transition(state, key)
+			p1 += key if c == '#' else 0
+			new_state[key] = c
 
-		printState(new_state, left_most-10, right_most + 20)
-		print('current answ: ', p1)
+		print(i, ' - current answ: ', p1, p1 - prev_p1)
 
+		prev_p1 = p1
 		state = new_state
 
+	# 5811 + (50000000000 - 1 - 88) * 62
 
 if __name__ == '__main__':
 	main()
